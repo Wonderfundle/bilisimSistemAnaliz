4f19e311f4585681764f40bea1faca16f1b9c0e1
nurhakbakici <nurhakbakici@gmail.com>
2023-12-21 02:57:00 +0300

    tedarikciler için php kodları eklendi

---
+++ tedarikciEkle.php
@@ -0,0 +1,36 @@
+<?php
+// veritabanina_ekle.php
+
+// Veritabanı bağlantısı
+$servername = "localhost"; // Veritabanı sunucusu adı
+$username = "root"; // Veritabanı kullanıcı adı
+$password = ""; // Veritabanı şifre
+$dbname = "bilisimAnalizProje"; // Veritabanı adı
+
+// Veritabanına bağlanma
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Bağlantıyı kontrol etme
+if ($conn->connect_error) {
+    die("Veritabanına bağlantı hatası: " . $conn->connect_error);
+}
+
+// POST verilerini alma
+$tedarikci_id = $_POST['tedarikci_id'];
+$tedarikci_adi = $_POST['tedarikci_adi'];
+$tedarikci_adresi = $_POST['tedarikci_adresi'];
+
+
+$sql = "INSERT INTO tedarikciler (tedarikci_id, tedarikci_adi, tedarikci_adresi VALUES ('$tedarikci_id', '$tedarikci_adi','$tedarikci_adres')";
+
+
+// Sorguyu çalıştırma ve sonucu kontrol etme
+if ($conn->query($sql) === TRUE) {
+    echo "Veri başarıyla eklendi";
+} else {
+    echo "Hata: " . $sql . "<br>" . $conn->error;
+}
+
+// Veritabanı bağlantısını kapatma
+$conn->close();
+?>
---
+++ tedarikciTablosu.php
@@ -0,0 +1,50 @@
+<?php
+// Veritabanı bağlantısı için bilgiler
+$servername = "localhost"; // Veritabanı sunucusu
+$username = "root"; // Veritabanı kullanıcı adı
+$password = ""; // Veritabanı şifre
+$dbname = "bilisimAnalizProje"; // Veritabanı adı
+
+// Veritabanına bağlan
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Bağlantıyı kontrol et
+if ($conn->connect_error) {
+    die("Bağlantı hatası: " . $conn->connect_error);
+}
+
+$sql = "SELECT tedarikci_id, tedarikci_adi, tedarikci_adres, FROM tedarikciler";
+$result = $conn->query($sql);
+
+// Veritabanı sorgusundan gelen verileri kullanarak tabloyu oluştur
+$table = '<table id="datatablesSimple" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Tedarikci ID</th>
+                    <th>Tedarikçi Adı</th>
+                    <th>Tedarikçi Adres</th>
+                </tr>
+            </thead>
+            <tbody>';
+
+if ($result->num_rows > 0) {
+    // Veritabanından gelen her bir satır için tabloya bir satır ekleyin
+    while ($row = $result->fetch_assoc()) {
+        $table .= '<tr>
+                      <td>' . $row['tedarikci_id'] . '</td>
+                      <td>' . $row['tedarikci_adi'] . '</td>
+                      <td>' . $row['tedarikci_adres'] . '</td>
+                   </tr>';
+    }
+} else {
+    $table .= '<tr><td colspan="4">Tabloda hiç veri yok.</td></tr>';
+}
+
+$table .= '</tbody>
+           </table>';
+
+// Veritabanı bağlantısını kapat
+$conn->close();
+
+// Oluşturulan tabloyu döndür
+?>
